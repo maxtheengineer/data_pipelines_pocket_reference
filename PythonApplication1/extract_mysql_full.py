c036d069ae311f8a77e1ebdb53a2887beaf4c397
maxtheengineer <maxtheengineer@max.be>
2026-05-28 17:53:33 +0200

    uploading to s3 bucket

--- PythonApplication1/extract_mysql_full.py
+++ PythonApplication1/extract_mysql_full.py
@@ -1,3 +1,5 @@
+from logging import config
+
 import boto3
 import csv
 import pymysql
@@ -32,3 +34,13 @@ with open(local_filename,'w') as fp:
     m_cursor.close()
     conn.close()
 
+# Uploading to a S3 Bucket
+access_key = parser.get('aws_boto_credentials','access_key')
+secret_key = parser.get('aws_boto_credentials','secret_key')
+bucket_name = parser.get('aws_boto_credentials','bucket_name')
+
+s3 = boto3.client('s3',aws_access_key_id=access_key,aws_secret_access_key=secret_key)
+
+s3_file = local_filename
+
+s3.upload_file(local_filename,bucket_name,s3_file)
